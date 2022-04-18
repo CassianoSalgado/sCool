184942b3e668567a3e102beddadbfaafc945c912
Cassinao Salgado <cassiano.salgado01@gmail.com>
2022-04-18 20:07:13 -0300

    Initial Commit

---
+++ sCool/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sCool
+//
+//  Created by Cassiano Carradore Salgado on 18/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
